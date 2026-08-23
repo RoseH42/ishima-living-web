074ed34969ef0b9063d01963041eb5a5012b3c82
Rose <rouroserabbit@gmail.com>
2026-08-23 23:43:24 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+
+// Swiper 
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 1,
+    spaceBetween: 24,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        992: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+    },
+});
